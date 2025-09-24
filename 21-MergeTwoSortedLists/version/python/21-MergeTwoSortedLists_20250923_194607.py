dd4ced27fb4226eab8982e14337fd27681eca047
Vaidik Vadhavana <63895478+VaidikV@users.noreply.github.com>
2025-09-23 19:46:07 -0700

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/version/python/21-MergeTwoSortedLists_20250923_194607.py
@@ -0,0 +1,21 @@
+# Last updated: 9/23/2025, 7:46:07 PM
+# This is the recursive approach. If list1 has smaller value, take the rest of list1 and og list2 and perform recursion and get the solution.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+
+        if list2 is None:
+            return list1
+
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
