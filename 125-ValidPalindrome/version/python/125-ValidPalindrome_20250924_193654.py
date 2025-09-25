e9aff81e13bfff0422d6492d1e97693aceddb60a
Vaidik Vadhavana <63895478+VaidikV@users.noreply.github.com>
2025-09-24 19:36:54 -0700

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/version/python/125-ValidPalindrome_20250924_193654.py
@@ -0,0 +1,21 @@
+# Last updated: 9/24/2025, 7:36:54 PM
+# Using a 2 pointer solution. The unique part is that we designed a function to see if a char is alphanum or not. Using ord().
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+        
+
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
