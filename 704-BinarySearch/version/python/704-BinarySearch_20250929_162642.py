9339a82478cfd4e83405d17fb6976c43e1a9f209
Vaidik Vadhavana <63895478+VaidikV@users.noreply.github.com>
2025-09-29 16:26:42 -0700

    Create 704-BinarySearch

---
+++ 704-BinarySearch/version/python/704-BinarySearch_20250929_162642.py
@@ -0,0 +1,16 @@
+# Last updated: 9/29/2025, 4:26:42 PM
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            middle = left + (right - left) // 2
+            
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] < target:
+                left = middle + 1
+            elif nums[middle] > target: 
+                right = middle - 1
+        
+        return -1
